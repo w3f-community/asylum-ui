5d1fac494937ddb2655012fb5a540483d068bbfb
mark savka <mark.savka.it@gmail.com>
2022-03-30 14:12:52 +0300

    refactor core

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Core } from './core';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Core />
   </React.StrictMode>,
   document.getElementById('root')
 );
