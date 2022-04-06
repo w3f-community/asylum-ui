4f9bc605e802ec106159586cb059b711e098ebd0
mark savka <mark.savka.it@gmail.com>
2022-04-06 13:22:42 +0300

    add basic page game-overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
+import { GameOverview } from './pages/game-overview'
 import { GameList } from './pages/game-selection'
 
 export const App = () => {
    return (
+      // eslint-disable-next-line max-len
       <div className="h-screen flex justify-center items-center bg-asylum-gray-1">
-         <GameList/>
+         {/* <GameList/> */}
+         <GameOverview />
       </div>
    )
 }
