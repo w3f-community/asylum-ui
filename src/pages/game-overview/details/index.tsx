7a9dd9452fa3cb898de060f4c0001fee53be2820
mark savka <mark.savka.it@gmail.com>
2022-04-12 10:46:45 +0300

    refactor detail header

--- src/pages/game-overview/details/index.tsx
+++ src/pages/game-overview/details/index.tsx
@@ -4,15 +4,16 @@ import { Card } from 'components/card'
 import { Heading } from 'components/text/heading'
 import { HeadingXl } from 'components/text/heading-xl'
 import { Paragraph } from 'components/text/paragraph'
+import { HeadingLg } from 'components/text/heading-lg'
 
 interface IProps { }
 
 export const Details: React.FC<IProps> = () => {
    return (
       <Card>
-         <HeadingXl className="mb-5">
+         <HeadingLg className="mb-5">
             Details
-         </HeadingXl>
+         </HeadingLg>
          <div className="flex flex-row justify-between">
             <div className="grow">
                <Heading>Number of players:</Heading>
